f435f95b2b4b133ebce2504cc1dbf3aada0de192
Felipe Lima <felipe.lima@gmail.com>
2021-02-04 21:19:40 -0800

    remove relevant tokens list and refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -30,20 +30,6 @@ declare global {
 }
 
 class App extends React.Component<any, AppState> {
-  relevantTokens = [
-    "DAI",
-    "LINK",
-    "UNI",
-    "AAVE",
-    "SUSHI",
-    "SNX",
-    "COMP",
-    "ZRX",
-    "FTM",
-    "MIR",
-    "1INCH",
-  ] as Array<string>;
-
   constructor(props: any) {
     super(props);
 
@@ -73,12 +59,28 @@ class App extends React.Component<any, AppState> {
 
   updateTokenBalance(token: string, balance: any) {
     if (this.state.web3) {
-      const { tokenBalances, web3 } = this.state;
-      tokenBalances[token] = web3.utils.fromWei(balance);
-      this.setState({ tokenBalances });
+      const tokenBalance = this.state.web3.utils.fromWei(balance);
+      if (+tokenBalance > 0) {
+        const { tokenBalances, allTokens } = this.state;
+        // TODO: Batch these one-off calls into a single API call with all tokens with balance > 0
+        tokenBalances[token] = tokenBalance;
+        this.fetchTokenPrices([allTokens[token].address]);
+        this.setState({ tokenBalances });
+      }
     } else {
       console.error("web3 is not yet initialized");
     }
+  }
+
+  async fetchTokenBalance(web3: Web3, token: Token) {
+    const { accountAddress } = this.state;
+    const tokenContractAddress = token.address;
+    const tokenPromise = new web3.eth.Contract(
+      humanStandardTokenABI as any,
+      tokenContractAddress
+    );
+    const balance = await tokenPromise.methods.balanceOf(accountAddress).call();
+    this.updateTokenBalance(token.symbol, balance);
   }
 
   async connectToMetaMask() {
@@ -89,34 +91,39 @@ class App extends React.Component<any, AppState> {
     const web3 = new Web3(window.ethereum);
     this.setState({ web3, accountAddress });
     web3.eth.getBalance(accountAddress).then(this.updateEthBalance.bind(this));
-    this.relevantTokens.forEach((token: string) => {
-      const tokenContractAddress = this.state.allTokens[token].address;
-      const tokenPromise = new web3.eth.Contract(
-        humanStandardTokenABI as any,
-        tokenContractAddress
-      );
-      tokenPromise.methods
-        .balanceOf(accountAddress)
-        .call()
-        .then((balance: any) => {
-          this.updateTokenBalance(token, balance);
-        });
-      this.fetchTokenPrice(tokenContractAddress).then((price) => {
-        const { tokenPrices } = this.state;
-        tokenPrices[token] = price;
-        this.setState({ tokenPrices });
-      });
+    Object.values(this.state.allTokens).forEach((token: Token) => {
+      this.fetchTokenBalance(web3, token);
     });
   }
 
-  async fetchTokenPrice(tokenAddress: string): Promise<string> {
-    return fetch(
-      `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${tokenAddress}&vs_currencies=usd`
-    )
+  findTokenByAddress(tokenAddress: string): Token | undefined {
+    const { allTokens } = this.state;
+    return Object.values(allTokens).find(
+      (t: Token) => t.address === tokenAddress
+    );
+  }
+
+  async fetchTokenPrices(tokenAddresses: Array<string>) {
+    const addressList = tokenAddresses.join(",");
+    const apiUrl = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${addressList}&vs_currencies=usd`;
+    fetch(apiUrl)
       .then((res) => res.json())
       .then((results) => {
-        const price = Object.values(results)[0] as any;
-        return price["usd"];
+        Object.entries(results).forEach(
+          ([tokenAddress, priceObj]: [string, any]) => {
+            const token = this.findTokenByAddress(tokenAddress);
+            if (token) {
+              const price = priceObj["usd"];
+              const { tokenPrices } = this.state;
+              tokenPrices[token.symbol] = price;
+              this.setState({ tokenPrices });
+            } else {
+              console.error(
+                `Unable to find token with address ${tokenAddress}`
+              );
+            }
+          }
+        );
       });
   }
 
@@ -183,8 +190,8 @@ class App extends React.Component<any, AppState> {
                     Deephy
                   </h1>
                   <p>
-                    Get a detailed balance of your DeFi tokens and get an
-                    account overview. Click the button below to get started.
+                    Get a detailed balance of your DeFi tokens and an account
+                    overview. Click the button below to get started.
                   </p>
                   {this.isMetamaskInstalled() && !this.state.web3 && (
                     <button
