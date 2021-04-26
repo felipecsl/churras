63ff1629b4d95b729dcfb41fb73bf4de1d037ee3
Felipe Lima <felipe.lima@gmail.com>
2021-04-25 20:46:44 -0700

    Another shot at setting provider correctly (#18)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { InfuraProvider } from "@ethersproject/providers";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Network } from "./chain";
+import { DEFAULT_BSC_PROVIDER } from "./constants";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import TokenBalanceResolver from "./token/tokenBalanceResolver";
 
+const INFURA_API_KEY = "3622ba9a27fe4e389875cd9c3e55670d";
+const ethereumProvider = InfuraProvider.getWebSocketProvider(
+  "homestead",
+  INFURA_API_KEY
+);
+const tokenBalanceResolver = new TokenBalanceResolver(
+  Object.fromEntries([
+    [Network[Network.ETHEREUM], ethereumProvider],
+    [Network[Network.BSC], DEFAULT_BSC_PROVIDER],
+  ])
+);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tokenBalanceResolver={tokenBalanceResolver} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
