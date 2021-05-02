d57950692720ecb29925aa064be3135a49532aca
Felipe Lima <felipe.lima@gmail.com>
2021-05-02 13:38:30 -0300

    refactor components

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ import React from "react";
 import AccountSnapshot from "./accountSnapshot";
 import "./App.css";
 import { Chain } from "./chain";
-import ThemeSelector from "./components/themeSelector";
 import TokenTableRow from "./components/tokenTableRow";
-import GithubLogo from "./images/github.svg";
-import Logo from "./images/logo.svg";
-import TwitterLogo from "./images/twitter.svg";
+import FooterComponent from "./footerComponent";
+import NavigationComponent from "./navigationComponent";
 import AccountCacheProvider from "./providers/accountCacheProvider";
 import DefaultMetaMaskProvider, {
   MetaMaskProvider,
@@ -167,44 +165,7 @@ class App extends React.Component<AppProps, AppState> {
     const showConnectToMetamaskButton = isMetaMaskInstalled && !accountAddress;
     return (
       <div className="flex flex-col h-screen justify-between">
-        <nav className="bg-gray-800 dark:bg-gray-700">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-16">
-              <div className="flex items-center">
-                <div className="flex-shrink-0 text-4xl">
-                  <a href="https://churras.org">
-                    <img src={Logo} alt="Churras logo" width="64" height="64" />
-                  </a>
-                </div>
-                <div className="hidden md:block">
-                  <div className="ml-10 flex items-start space-x-4">
-                    <a
-                      href="/"
-                      className="bg-gray-900 text-white px-3 py-3 rounded-md text-sm font-medium"
-                    >
-                      Dashboard
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-4 flex items-center md:ml-6">
-                  <ThemeSelector />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="md:hidden" id="mobile-menu">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <a
-                href="/"
-                className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Dashboard
-              </a>
-            </div>
-          </div>
-        </nav>
+        <NavigationComponent />
         <header className="bg-white dark:bg-gray-800 shadow">
           <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-300">
@@ -308,95 +269,7 @@ class App extends React.Component<AppProps, AppState> {
             </div>
           </div>
         </main>
-        <footer className="footer bg-white dark:text-gray-300 dark:bg-gray-700 relative pt-1 border-b-2 border-gray-700">
-          <div className="container max-w-7xl mx-auto px-6">
-            <div className="sm:flex sm:mt-8">
-              <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 grid grid-cols-2 justify-items-start">
-                <div>
-                  <div className="my-2 text-5xl">
-                    <a href="https://churras.org">
-                      <img
-                        src={Logo}
-                        alt="Churras logo"
-                        width="64"
-                        height="64"
-                      />
-                    </a>
-                  </div>
-                  <div className="my-5">
-                    <p className="leading-relaxed">
-                      Churras is your DeFi wallet dashboard. <br /> It supports
-                      both Ethereum and Binance Smart Chain. <br /> Track your
-                      account balance, tokens, yields and <br /> be your own
-                      bank.
-                    </p>
-                  </div>
-                  <div className="my-5 flex">
-                    <a
-                      href="https://github.com/felipecsl/churras"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="flex mr-4"
-                    >
-                      <img
-                        src={GithubLogo}
-                        alt="Churras Github"
-                        className="fill-current filter dark:invert"
-                      />
-                    </a>
-                    <a
-                      href="https://twitter.com/churras_org"
-                      target="_blank"
-                      rel="noreferrer"
-                      className="flex"
-                    >
-                      <img
-                        src={TwitterLogo}
-                        alt="Churras Twitter"
-                        className="fill-current filter dark:invert"
-                      />
-                    </a>
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold text-gray-900 dark:text-gray-200 uppercase my-8">
-                    About
-                  </div>
-                  <div className="my-4">
-                    <a href="https://docs.churras.org/" className="text-md">
-                      Documentation
-                    </a>
-                  </div>
-                  <div className="my-4">
-                    <a
-                      href="https://github.com/felipecsl/churras"
-                      className="text-md"
-                    >
-                      Source Code
-                    </a>
-                  </div>
-                  <div className="my-4">
-                    <a href="mailto:felipe.lima@gmail.com" className="text-md">
-                      Contact
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container mx-auto px-6">
-            <div className="mt-16 border-t-2 border-gray-200 dark:border-gray-600 flex flex-col items-center">
-              <div className="sm:w-2/3 text-center py-6">
-                <p className="text-sm font-bold mb-2">
-                  Built with ☕️ by{" "}
-                  <a className="underline" href="https://felipecsl.com">
-                    Felipe Lima
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <FooterComponent />
       </div>
     );
   }
