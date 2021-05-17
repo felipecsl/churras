3103d00983ce932bbd89446e2ec8eb0dccdf5b65
Felipe Lima <felipe.lima@gmail.com>
2021-05-16 23:08:08 -0300

    revert this file

--- src/api/providers/bsc/bscAccountTokensProvider.ts
+++ src/api/providers/bsc/bscAccountTokensProvider.ts
@@ -1,25 +1,14 @@
-import { BaseProvider } from "@ethersproject/providers";
-import { utils } from "ethers";
-import { hexZeroPad } from "ethers/lib/utils";
 import { Network } from "../../../chain";
-import {
-  NetworkProviderFactory,
-  TokenDatabaseFactory,
-} from "../../modulesProvider";
+import { TokenDatabaseFactory } from "../../modulesProvider";
 import Token from "../../token/token";
 import TokenDatabase from "../../token/tokenDatabase";
 import { AccountTokensProvider } from "../accountTokensProvider";
 
 export default class BscAccountTokensProvider implements AccountTokensProvider {
   private readonly tokenDatabase: TokenDatabase;
-  private readonly bscProvider: BaseProvider;
 
-  constructor(
-    tokenDatabaseFactory: TokenDatabaseFactory,
-    networkProviderFactory: NetworkProviderFactory
-  ) {
+  constructor(tokenDatabaseFactory: TokenDatabaseFactory) {
     this.tokenDatabase = tokenDatabaseFactory(Network[Network.BSC]);
-    this.bscProvider = networkProviderFactory(Network[Network.BSC]);
   }
 
   async accountTokens(accountAddress: string): Promise<Token[]> {
@@ -29,22 +18,6 @@ export default class BscAccountTokensProvider implements AccountTokensProvider {
     // events, like we do on Ethereum. As a last resort, we return all BSC database tokens, since
     // we can't tell whether or not any of them are relevant for the provided account, so we have
     // to play it safe and query all of them (ewww)...
-    // return Promise.resolve(this.tokenDatabase.allTokens());
-    // Filter all token transfers from any address to `accountAddress`
-    var filter = {
-      topics: [
-        utils.id("Transfer(address,address,uint256)"),
-        null,
-        hexZeroPad(accountAddress, 32),
-      ] as string[],
-      fromBlock: 0,
-      toBlock: "latest",
-    };
-    const logs = await this.bscProvider.getLogs(filter);
-    // Any ERC-721 tokens would fail to `findByAddress` since they are not in the tokenDatabase,
-    // so for now we just filter them out.
-    return [...new Set(logs.map((log) => log.address))]
-      .map((address) => this.tokenDatabase.findByAddress(address))
-      .filter((token) => token !== undefined) as Token[];
+    return Promise.resolve(this.tokenDatabase.allTokens());
   }
 }
