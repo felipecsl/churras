7d3dae8dad0c80b64e703aed0bcfd7e49a9e22c9
Felipe Lima <felipe.lima@gmail.com>
2021-05-27 15:19:14 -0700

    switch to esbuild

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,7 @@
+const CracoEsbuildPlugin = require('craco-esbuild');
+
 module.exports = {
+  plugins: [{ plugin: CracoEsbuildPlugin }],
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
